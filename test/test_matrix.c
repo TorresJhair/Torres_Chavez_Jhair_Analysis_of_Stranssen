0a42c5353bd076fe0db0b8db70c63acb0d1aa7e7
Torres Chavez Jhair <jtorreschav@unsa.edu.pe>
2026-04-30 23:48:38 -0500

    test (matrix): verificar correcto funcionamiento de la reserva y liberación de la memoria (MATRIZ CON DOBLE PUNTERO)

---
+++ test/test_matrix.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "matrix.h"
+
+// Debería reservar y liberar memoria correctamente
+// Verificar manejo memoria, no importa los valores dentro de la matriz
+void test_alloc_free() {
+    int n = 4;
+    double **A = allocMatrix(n);
+
+    assert(A != NULL);
+
+    for (int i = 0; i < n; i++)
+        assert(A[i] != NULL);
+
+    freeMatrix(A, n);
+    printf("Test alloc/free: OK\n");
+}
+
+int main() {
+    printf("Tests Matrix\n");
+
+    test_alloc_free();
+
+    printf("Todos los Test de Matrix exitosos =)\n");
+    return;
+}
