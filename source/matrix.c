dfac41059e5b20875d6e18d7fbd1e170d6b2872b
Torres Chavez Jhair <jtorreschav@unsa.edu.pe>
2026-04-30 23:40:00 -0500

    add (source): implementación de las funciones básicas para matrices

---
+++ source/matrix.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include "matrix.h"
+
+double** allocMatrix(int n) {
+    double** M = malloc(n * sizeof(double*));
+    for (int i = 0; i < n; i++)
+        M[i] = malloc(n * sizeof(double));
+    return M;
+}
+
+void freeMatrix(double** M, int n) {
+    for (int i = 0; i < n; i++)
+        free(M[i]);
+    free(M);
+}
+
+void copyMatrix(double** A, double** B, int n) {
+    for (int i = 0; i < n; i++)
+        memcpy(B[i], A[i], n * sizeof(double));
+}
+
+void randomMatrix(double** A, int n) {
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            A[i][j] = rand() % 10;
+}
