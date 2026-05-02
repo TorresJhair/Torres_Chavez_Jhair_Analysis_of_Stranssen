c0a8c5da9acbab64d702f542e3380ed116dfe8e0
Torres Chavez Jhair <jtorreschav@unsa.edu.pe>
2026-05-02 17:16:36 -0500

    test(matrix): test unitarios para la suma y resta de matrices, verificando que tome las mastrices A y B, las reste o suma según sea el caso, y deje el resultado en la matriz C

--- test/test_matrix.c
+++ test/test_matrix.c
@@ -84,6 +84,66 @@ void test_zero_matrix() {
     freeMatrix(M, n);
 }
 
+void test_add_matrix() {
+    int n = 3;
+
+    double** A = allocMatrix(n);
+    double** B = allocMatrix(n);
+    double** C = allocMatrix(n);
+    double** expected = allocMatrix(n);
+
+    A[0][0] = 1.0; A[0][1] = 2.0; A[0][2] = 3.0;
+    A[1][0] = 4.0; A[1][1] = 5.0; A[1][2] = 6.0;
+    A[2][0] = 7.0; A[2][1] = 8.0; A[2][2] = 9.0;
+
+    B[0][0] = 9.0; B[0][1] = 8.0; B[0][2] = 7.0;
+    B[1][0] = 6.0; B[1][1] = 5.0; B[1][2] = 4.0;
+    B[2][0] = 3.0; B[2][1] = 2.0; B[2][2] = 1.0;
+
+    expected[0][0] = 10.0; expected[0][1] = 10.0; expected[0][2] = 10.0;
+    expected[1][0] = 10.0; expected[1][1] = 10.0; expected[1][2] = 10.0;
+    expected[2][0] = 10.0; expected[2][1] = 10.0; expected[2][2] = 10.0;
+
+    addMatrix(A, B, C, n);
+
+    assert(equalMatrix(expected, C, n));
+
+    freeMatrix(A, n);
+    freeMatrix(B, n);
+    freeMatrix(C, n);
+    freeMatrix(expected, n);
+}
+
+void test_sub_matrix() {
+    int n = 3;
+
+    double** A = allocMatrix(n);
+    double** B = allocMatrix(n);
+    double** C = allocMatrix(n);
+    double** expected = allocMatrix(n);
+
+    A[0][0] = 5.0; A[0][1] = 7.0; A[0][2] = 9.0;
+    A[1][0] = 3.0; A[1][1] = 2.0; A[1][2] = 6.0;
+    A[2][0] = 8.0; A[2][1] = 1.0; A[2][2] = 4.0;
+
+    B[0][0] = 2.0; B[0][1] = 3.0; B[0][2] = 1.0;
+    B[1][0] = 1.0; B[1][1] = 1.0; B[1][2] = 2.0;
+    B[2][0] = 4.0; B[2][1] = 0.0; B[2][2] = 3.0;
+
+    expected[0][0] = 3.0; expected[0][1] = 4.0; expected[0][2] = 8.0;
+    expected[1][0] = 2.0; expected[1][1] = 1.0; expected[1][2] = 4.0;
+    expected[2][0] = 4.0; expected[2][1] = 1.0; expected[2][2] = 1.0;
+
+    subMatrix(A, B, C, n);
+
+    assert(equalMatrix(expected, C, n));
+
+    freeMatrix(A, n);
+    freeMatrix(B, n);
+    freeMatrix(C, n);
+    freeMatrix(expected, n);
+}
+
 int test_main_matrix() {
     printf("Tests Matrix\n");
 
@@ -91,6 +151,8 @@ int test_main_matrix() {
     test_copy_matrix();
     test_random_matrix_range();
     test_zero_matrix();
+    test_add_matrix();
+    test_sub_matrix();
 
     printf("Todos los Test de Matrix exitosos =)\n");
     return 0;
