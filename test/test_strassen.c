6bb589afb2649ce2f43091905b2eb1a5d1911784
Torres Chavez Jhair <jtorreschav@unsa.edu.pe>
2026-05-04 18:59:43 -0500

    test(strassen): actualización de los tests debido al reciente cambio de implementación de puntero doble a puntero simple

--- test/test_strassen.c
+++ test/test_strassen.c
@@ -10,39 +10,39 @@
 void test_strassen_2x2() {
     int n = 2;
 
-    double** A = allocMatrix(n);
-    double** B = allocMatrix(n);
-    double** C = allocMatrix(n);
+    double* A = allocMatrix(n);
+    double* B = allocMatrix(n);
+    double* C = allocMatrix(n);
 
-    A[0][0] = 1.0; A[0][1] = 2.0;
-    A[1][0] = 3.0; A[1][1] = 4.0;
+    A[0*n + 0] = 1.0; A[0*n + 1] = 2.0;
+    A[1*n + 0] = 3.0; A[1*n + 1] = 4.0;
 
-    B[0][0] = 5.0; B[0][1] = 6.0;
-    B[1][0] = 7.0; B[1][1] = 8.0;
+    B[0*n + 0] = 5.0; B[0*n + 1] = 6.0;
+    B[1*n + 0] = 7.0; B[1*n + 1] = 8.0;
 
     strassen(A, B, C, n);
 
-    double** result = allocMatrix(n);
-    result[0][0] = 19.0;
-    result[0][1] = 22.0;
-    result[1][0] = 43.0;
-    result[1][1] = 50.0;
+    double* result = allocMatrix(n);
+    result[0*n + 0] = 19.0;
+    result[0*n + 1] = 22.0;
+    result[1*n + 0] = 43.0;
+    result[1*n + 1] = 50.0;
 
     assert(equalMatrix(result, C, n));
 
-    freeMatrix(A, n);
-    freeMatrix(B, n);
-    freeMatrix(C, n);
-    freeMatrix(result, n);
+    freeMatrix(A);
+    freeMatrix(B);
+    freeMatrix(C);
+    freeMatrix(result);
 }
 
 void test_strassen_4x4() {
     int n = 4;
 
-    double** A = allocMatrix(n);
-    double** B = allocMatrix(n);
-    double** C_strassen = allocMatrix(n);
-    double** C_classic = allocMatrix(n);
+    double* A = allocMatrix(n);
+    double* B = allocMatrix(n);
+    double* C_strassen = allocMatrix(n);
+    double* C_classic = allocMatrix(n);
 
     srand(10);
     randomMatrix(A, n);
@@ -53,41 +53,41 @@ void test_strassen_4x4() {
 
     assert(equalMatrix(C_strassen, C_classic, n));
 
-    freeMatrix(A, n);
-    freeMatrix(B, n);
-    freeMatrix(C_strassen, n);
-    freeMatrix(C_classic, n);
+    freeMatrix(A);
+    freeMatrix(B);
+    freeMatrix(C_strassen);
+    freeMatrix(C_classic);
 }
 
 void test_strassen_identity() {
     int n = 4;
 
-    double** A = allocMatrix(n);
-    double** I = allocMatrix(n);
-    double** C = allocMatrix(n);
+    double* A = allocMatrix(n);
+    double* I = allocMatrix(n);
+    double* C = allocMatrix(n);
 
     srand(20);
     randomMatrix(A, n);
 
     zeroMatrix(I, n);
     for (int i = 0; i < n; i++)
-        I[i][i] = 1.0;
+        I[i*n + i] = 1.0;
 
     strassen(A, I, C, n);
 
     assert(equalMatrix(A, C, n));
 
-    freeMatrix(A, n);
-    freeMatrix(I, n);
-    freeMatrix(C, n);
+    freeMatrix(A);
+    freeMatrix(I);
+    freeMatrix(C);
 }
 
 void test_strassen_zero_matrix() {
     int n = 4;
 
-    double** A = allocMatrix(n);
-    double** Z = allocMatrix(n);
-    double** C = allocMatrix(n);
+    double* A = allocMatrix(n);
+    double* Z = allocMatrix(n);
+    double* C = allocMatrix(n);
 
     srand(30);
     randomMatrix(A, n);
@@ -98,18 +98,18 @@ void test_strassen_zero_matrix() {
 
     assert(equalMatrix(Z, C, n));
 
-    freeMatrix(A, n);
-    freeMatrix(Z, n);
-    freeMatrix(C, n);
+    freeMatrix(A);
+    freeMatrix(Z);
+    freeMatrix(C);
 }
 
 void test_strassen_5x5() {
     int n = 5;
 
-    double** A = allocMatrix(n);
-    double** B = allocMatrix(n);
-    double** C_strassen = allocMatrix(n);
-    double** C_classic = allocMatrix(n);
+    double* A = allocMatrix(n);
+    double* B = allocMatrix(n);
+    double* C_strassen = allocMatrix(n);
+    double* C_classic = allocMatrix(n);
 
     srand(42);
     randomMatrix(A, n);
@@ -120,10 +120,10 @@ void test_strassen_5x5() {
 
     assert(equalMatrix(C_strassen, C_classic, n));
 
-    freeMatrix(A, n);
-    freeMatrix(B, n);
-    freeMatrix(C_strassen, n);
-    freeMatrix(C_classic, n);
+    freeMatrix(A);
+    freeMatrix(B);
+    freeMatrix(C_strassen);
+    freeMatrix(C_classic);
 }
 
 int test_main_strassen() {
