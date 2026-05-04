0f115a0a4cb104e600e143d69984121cb1554c8f
Torres Chavez Jhair <jtorreschav@unsa.edu.pe>
2026-05-04 18:22:50 -0500

    test(mult_classic): actualización de los tests debido al reciente cambio de implementación de puntero doble a puntero simple

--- test/test_mult_classic.c
+++ test/test_mult_classic.c
@@ -12,33 +12,32 @@
 void test_multiplicacion_2x2() {
     int n = 2;
 
-    double** A = allocMatrix(n);
-    double** B = allocMatrix(n);
-    double** C = allocMatrix(n);
+    double* A = allocMatrix(n);
+    double* B = allocMatrix(n);
+    double* C = allocMatrix(n);
 
     // A = [1 2; 3 4]
-    A[0][0] = 1.0; A[0][1] = 2.0;
-    A[1][0] = 3.0; A[1][1] = 4.0;
+    A[0 * n + 0] = 1.0; A[0 * n + 1] = 2.0;
+    A[1 * n + 0] = 3.0; A[1 * n + 1] = 4.0;
 
     // B = [5 6; 7 8]
-    B[0][0] = 5.0; B[0][1] = 6.0;
-    B[1][0] = 7.0; B[1][1] = 8.0;
+    B[0 * n + 0] = 5.0; B[0 * n + 1] = 6.0;
+    B[1 * n + 0] = 7.0; B[1 * n + 1] = 8.0;
 
     mult_classic(A, B, C, n);
 
-    double** result = allocMatrix(n);
-    result[0][0] = 19.0;
-    result[0][1] = 22.0;
-    result[1][0] = 43.0;
-    result[1][1] = 50.0;
+    double* result = allocMatrix(n);
+    result[0 * n + 0] = 19.0;
+    result[0 * n + 1] = 22.0;
+    result[1 * n + 0] = 43.0;
+    result[1 * n + 1] = 50.0;
 
     assert(equalMatrix(result, C, n));
 
-    freeMatrix(A,n);
-    freeMatrix(B,n);
-    freeMatrix(C,n);
-    freeMatrix(result,n);
-
+    freeMatrix(A);
+    freeMatrix(B);
+    freeMatrix(C);
+    freeMatrix(result);
 }
 
 // -----------------------------
@@ -47,24 +46,24 @@ void test_multiplicacion_2x2() {
 void test_identity() {
     int n = 4;
 
-    double** A = allocMatrix(n);
-    double** I = allocMatrix(n);
-    double** C = allocMatrix(n);
+    double* A = allocMatrix(n);
+    double* I = allocMatrix(n);
+    double* C = allocMatrix(n);
 
     srand(1);
     randomMatrix(A, n);
 
     zeroMatrix(I, n);
     for (int i = 0; i < n; i++)
-        I[i][i] = 1.0;
+        I[i * n + i] = 1.0;
 
     mult_classic(A, I, C, n);
 
     assert(equalMatrix(A, C, n));
 
-    freeMatrix(A,n);
-    freeMatrix(I,n);
-    freeMatrix(C,n);
+    freeMatrix(A);
+    freeMatrix(I);
+    freeMatrix(C);
 }
 
 // -----------------------------
@@ -73,9 +72,9 @@ void test_identity() {
 void test_zero_matrix_mult() {
     int n = 4;
 
-    double** A = allocMatrix(n);
-    double** Z = allocMatrix(n);
-    double** C = allocMatrix(n);
+    double* A = allocMatrix(n);
+    double* Z = allocMatrix(n);
+    double* C = allocMatrix(n);
 
     srand(2);
     randomMatrix(A, n);
@@ -86,9 +85,9 @@ void test_zero_matrix_mult() {
 
     assert(equalMatrix(Z, C, n));
 
-    freeMatrix(A,n);
-    freeMatrix(Z,n);
-    freeMatrix(C,n);
+    freeMatrix(A);
+    freeMatrix(Z);
+    freeMatrix(C);
 }
 
 // -----------------------------
@@ -97,37 +96,37 @@ void test_zero_matrix_mult() {
 void test_multiplicacion_5x5() {
     int n = 5;
 
-    double** A = allocMatrix(n);
-    double** B = allocMatrix(n);
-    double** C = allocMatrix(n);
+    double* A = allocMatrix(n);
+    double* B = allocMatrix(n);
+    double* C = allocMatrix(n);
 
-    A[0][0] = 1.0; A[0][1] = 2.0; A[0][2] = 3.0; A[0][3] = 4.0; A[0][4] = 5.0;
-    A[1][0] = 6.0; A[1][1] = 7.0; A[1][2] = 8.0; A[1][3] = 9.0; A[1][4] = 10.0;
-    A[2][0] = 11.0; A[2][1] = 12.0; A[2][2] = 13.0; A[2][3] = 14.0; A[2][4] = 15.0;
-    A[3][0] = 16.0; A[3][1] = 17.0; A[3][2] = 18.0; A[3][3] = 19.0; A[3][4] = 20.0;
-    A[4][0] = 21.0; A[4][1] = 22.0; A[4][2] = 23.0; A[4][3] = 24.0; A[4][4] = 25.0;
+    A[0 * n + 0] = 1.0; A[0 * n + 1] = 2.0; A[0 * n + 2] = 3.0; A[0 * n + 3] = 4.0; A[0 * n + 4] = 5.0;
+    A[1 * n + 0] = 6.0; A[1 * n + 1] = 7.0; A[1 * n + 2] = 8.0; A[1 * n + 3] = 9.0; A[1 * n + 4] = 10.0;
+    A[2 * n + 0] = 11.0; A[2 * n + 1] = 12.0; A[2 * n + 2] = 13.0; A[2 * n + 3] = 14.0; A[2 * n + 4] = 15.0;
+    A[3 * n + 0] = 16.0; A[3 * n + 1] = 17.0; A[3 * n + 2] = 18.0; A[3 * n + 3] = 19.0; A[3 * n + 4] = 20.0;
+    A[4 * n + 0] = 21.0; A[4 * n + 1] = 22.0; A[4 * n + 2] = 23.0; A[4 * n + 3] = 24.0; A[4 * n + 4] = 25.0;
 
-    B[0][0] = 25.0; B[0][1] = 24.0; B[0][2] = 23.0; B[0][3] = 22.0; B[0][4] = 21.0;
-    B[1][0] = 20.0; B[1][1] = 19.0; B[1][2] = 18.0; B[1][3] = 17.0; B[1][4] = 16.0;
-    B[2][0] = 15.0; B[2][1] = 14.0; B[2][2] = 13.0; B[2][3] = 12.0; B[2][4] = 11.0;
-    B[3][0] = 10.0; B[3][1] = 9.0; B[3][2] = 8.0; B[3][3] = 7.0; B[3][4] = 6.0;
-    B[4][0] = 5.0; B[4][1] = 4.0; B[4][2] = 3.0; B[4][3] = 2.0; B[4][4] = 1.0;
+    B[0 * n + 0] = 25.0; B[0 * n + 1] = 24.0; B[0 * n + 2] = 23.0; B[0 * n + 3] = 22.0; B[0 * n + 4] = 21.0;
+    B[1 * n + 0] = 20.0; B[1 * n + 1] = 19.0; B[1 * n + 2] = 18.0; B[1 * n + 3] = 17.0; B[1 * n + 4] = 16.0;
+    B[2 * n + 0] = 15.0; B[2 * n + 1] = 14.0; B[2 * n + 2] = 13.0; B[2 * n + 3] = 12.0; B[2 * n + 4] = 11.0;
+    B[3 * n + 0] = 10.0; B[3 * n + 1] = 9.0; B[3 * n + 2] = 8.0; B[3 * n + 3] = 7.0; B[3 * n + 4] = 6.0;
+    B[4 * n + 0] = 5.0; B[4 * n + 1] = 4.0; B[4 * n + 2] = 3.0; B[4 * n + 3] = 2.0; B[4 * n + 4] = 1.0;
 
     mult_classic(A, B, C, n);
 
-    double** expected = allocMatrix(n);
-    expected[0][0] = 175.0; expected[0][1] = 160.0; expected[0][2] = 145.0; expected[0][3] = 130.0; expected[0][4] = 115.0;
-    expected[1][0] = 550.0; expected[1][1] = 510.0; expected[1][2] = 470.0; expected[1][3] = 430.0; expected[1][4] = 390.0;
-    expected[2][0] = 925.0; expected[2][1] = 860.0; expected[2][2] = 795.0; expected[2][3] = 730.0; expected[2][4] = 665.0;
-    expected[3][0] = 1300.0; expected[3][1] = 1210.0; expected[3][2] = 1120.0; expected[3][3] = 1030.0; expected[3][4] = 940.0;
-    expected[4][0] = 1675.0; expected[4][1] = 1560.0; expected[4][2] = 1445.0; expected[4][3] = 1330.0; expected[4][4] = 1215.0;
+    double* expected = allocMatrix(n);
+    expected[0 * n + 0] = 175.0; expected[0 * n + 1] = 160.0; expected[0 * n + 2] = 145.0; expected[0 * n + 3] = 130.0; expected[0 * n + 4] = 115.0;
+    expected[1 * n + 0] = 550.0; expected[1 * n + 1] = 510.0; expected[1 * n + 2] = 470.0; expected[1 * n + 3] = 430.0; expected[1 * n + 4] = 390.0;
+    expected[2 * n + 0] = 925.0; expected[2 * n + 1] = 860.0; expected[2 * n + 2] = 795.0; expected[2 * n + 3] = 730.0; expected[2 * n + 4] = 665.0;
+    expected[3 * n + 0] = 1300.0; expected[3 * n + 1] = 1210.0; expected[3 * n + 2] = 1120.0; expected[3 * n + 3] = 1030.0; expected[3 * n + 4] = 940.0;
+    expected[4 * n + 0] = 1675.0; expected[4 * n + 1] = 1560.0; expected[4 * n + 2] = 1445.0; expected[4 * n + 3] = 1330.0; expected[4 * n + 4] = 1215.0;
 
     assert(equalMatrix(expected, C, n));
 
-    freeMatrix(A, n);
-    freeMatrix(B, n);
-    freeMatrix(C, n);
-    freeMatrix(expected, n);
+    freeMatrix(A);
+    freeMatrix(B);
+    freeMatrix(C);
+    freeMatrix(expected);
 }
 
 // -----------------------------
@@ -139,6 +138,6 @@ int test_main_mult_matrix() {
     test_zero_matrix_mult();
     test_multiplicacion_5x5();
 
-    printf("Todos los Tests de mult_classic existosos =)\n");
+    printf("Todos los Tests de mult_classic exitosos =)\n");
     return 0;
 }
