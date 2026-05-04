e919a837cdb512358112f3dfca940a91b8322cf0
Torres Chavez Jhair <jtorreschav@unsa.edu.pe>
2026-05-04 18:22:16 -0500

    refactor(mult_classic): cambio de implementación de puntero doble a puntero simple, uso de aritmética de punteros para la separación lógica de filas y columnas

--- interfaces/mult_classic.h
+++ interfaces/mult_classic.h
@@ -1,6 +1,6 @@
 #ifndef MULT_CLASSIC_H
 #define MULT_CLASSIC_H
 
-void mult_classic(double** A, double** B, double** C, int n);
+void mult_classic(double* A, double* B, double* C, int n);
 
 #endif
--- source/mult_classic.c
+++ source/mult_classic.c
@@ -1,11 +1,11 @@
 #include "../interfaces/matrix.h"
 #include "../interfaces/mult_classic.h"
 
-void mult_classic(double** A, double** B, double** C, int n) {
+void mult_classic(double* A, double* B, double* C, int n) {
     // Matrix C, completamente nueva, rellena de 0's
     zeroMatrix(C, n);
     for (int i = 0; i < n; i++)
         for (int k = 0; k < n; k++)
             for (int j = 0; j < n; j++)
-                C[i][j] += A[i][k] * B[k][j];
+                C[i * n + j] += A[i * n + k] * B[k * n + j];
 }
